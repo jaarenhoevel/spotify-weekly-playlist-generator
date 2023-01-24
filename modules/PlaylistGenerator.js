61bff1fc9810d382b6c9961ccbe516e979996ad5
János Arenhövel <jaarenhoevel@gmail.com>
2023-01-24 21:59:38 +0100

    Add all missing tracks to song list with some basic parameters needed for sorting later on

--- modules/PlaylistGenerator.js
+++ modules/PlaylistGenerator.js
@@ -12,5 +12,20 @@ export async function refreshSongList(spotifyApi, playlistId, songs) {
         offset += 25;
     } while (response.body.next !== null);
 
-    console.log(tracks);
+    var addedTracks = 0;
+
+    tracks.forEach(track => {
+        if (!(track.track.id in songs) && !track.is_local) { // Track is not in song list yet (and not just a local song)
+            songs[track.track.id] = {
+                addedAt: track.added_at,
+                artistId: track.track.album.artists[0].id,
+                releaseDate: track.track.album.release_date,
+                lastSelected: null
+            };
+
+            addedTracks ++;
+        }
+    });
+
+    console.log(`Added ${addedTracks} tracks to song list.`);
 }
